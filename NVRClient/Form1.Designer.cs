07a0bb728a24a52ccb69b51a172315d0b36a1808
ritwickhanda <ritwickhanda2@gmail.com>
2026-01-14 16:58:34 +0530

    Add project files.

---
+++ NVRClient/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace NVRClient
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtBaseUrl = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.cmbCameraId = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnPlayback = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnPlayLive = new System.Windows.Forms.Button();
+            this.btnStartListener = new System.Windows.Forms.Button();
+            this.btnStopListener = new System.Windows.Forms.Button();
+            this.videoPanel = new System.Windows.Forms.Panel();
+            this.dtFrom = new System.Windows.Forms.DateTimePicker();
+            this.dtTo = new System.Windows.Forms.DateTimePicker();
+            this.lblBaseUrl = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblCameraId = new System.Windows.Forms.Label();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.grpPTZ = new System.Windows.Forms.GroupBox();
+            this.btnPTZUp = new System.Windows.Forms.Button();
+            this.btnPTZDown = new System.Windows.Forms.Button();
+            this.btnPTZLeft = new System.Windows.Forms.Button();
+            this.btnPTZRight = new System.Windows.Forms.Button();
+            this.btnZoomIn = new System.Windows.Forms.Button();
+            this.btnZoomOut = new System.Windows.Forms.Button();
+            this.grpPTZ.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtBaseUrl
+            // 
+            this.txtBaseUrl.Location = new System.Drawing.Point(80, 12);
+            this.txtBaseUrl.Name = "txtBaseUrl";
+            this.txtBaseUrl.Size = new System.Drawing.Size(160, 20);
+            this.txtBaseUrl.TabIndex = 1;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(320, 12);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(500, 12);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 5;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // cmbCameraId
+            // 
+            this.cmbCameraId.Location = new System.Drawing.Point(80, 47);
+            this.cmbCameraId.Name = "cmbCameraId";
+            this.cmbCameraId.Size = new System.Drawing.Size(160, 21);
+            this.cmbCameraId.TabIndex = 9;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(620, 10);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 6;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click_1);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(720, 10);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect.TabIndex = 7;
+            this.btnDisconnect.Text = "Disconnect";
+            // 
+            // btnPlayback
+            // 
+            this.btnPlayback.Location = new System.Drawing.Point(80, 80);
+            this.btnPlayback.Name = "btnPlayback";
+            this.btnPlayback.Size = new System.Drawing.Size(75, 23);
+            this.btnPlayback.TabIndex = 10;
+            this.btnPlayback.Text = "Play Back";
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(170, 80);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 11;
+            this.btnExport.Text = "Export";
+            // 
+            // btnPlayLive
+            // 
+            this.btnPlayLive.Location = new System.Drawing.Point(250, 80);
+            this.btnPlayLive.Name = "btnPlayLive";
+            this.btnPlayLive.Size = new System.Drawing.Size(75, 23);
+            this.btnPlayLive.TabIndex = 12;
+            this.btnPlayLive.Text = "Play Live";
+            // 
+            // btnStartListener
+            // 
+            this.btnStartListener.Location = new System.Drawing.Point(12, 120);
+            this.btnStartListener.Name = "btnStartListener";
+            this.btnStartListener.Size = new System.Drawing.Size(75, 23);
+            this.btnStartListener.TabIndex = 13;
+            this.btnStartListener.Text = "Start Listener";
+            // 
+            // btnStopListener
+            // 
+            this.btnStopListener.Location = new System.Drawing.Point(12, 155);
+            this.btnStopListener.Name = "btnStopListener";
+            this.btnStopListener.Size = new System.Drawing.Size(75, 23);
+            this.btnStopListener.TabIndex = 14;
+            this.btnStopListener.Text = "Stop Listener";
+            // 
+            // videoPanel
+            // 
+            this.videoPanel.Location = new System.Drawing.Point(200, 120);
+            this.videoPanel.Name = "videoPanel";
+            this.videoPanel.Size = new System.Drawing.Size(518, 345);
+            this.videoPanel.TabIndex = 19;
+            // 
+            // dtFrom
+            // 
+            this.dtFrom.Location = new System.Drawing.Point(12, 220);
+            this.dtFrom.Name = "dtFrom";
+            this.dtFrom.Size = new System.Drawing.Size(182, 20);
+            this.dtFrom.TabIndex = 16;
+            // 
+            // dtTo
+            // 
+            this.dtTo.Location = new System.Drawing.Point(12, 270);
+            this.dtTo.Name = "dtTo";
+            this.dtTo.Size = new System.Drawing.Size(182, 20);
+            this.dtTo.TabIndex = 18;
+            // 
+            // lblBaseUrl
+            // 
+            this.lblBaseUrl.Location = new System.Drawing.Point(12, 10);
+            this.lblBaseUrl.Name = "lblBaseUrl";
+            this.lblBaseUrl.Size = new System.Drawing.Size(62, 23);
+            this.lblBaseUrl.TabIndex = 0;
+            this.lblBaseUrl.Text = "Base URL";
+            this.lblBaseUrl.Click += new System.EventHandler(this.lblBaseUrl_Click);
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.Location = new System.Drawing.Point(243, 12);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(75, 23);
+            this.lblUsername.TabIndex = 2;
+            this.lblUsername.Text = "Username";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.Location = new System.Drawing.Point(426, 12);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(68, 23);
+            this.lblPassword.TabIndex = 4;
+            this.lblPassword.Text = "Password";
+            // 
+            // lblCameraId
+            // 
+            this.lblCameraId.Location = new System.Drawing.Point(12, 47);
+            this.lblCameraId.Name = "lblCameraId";
+            this.lblCameraId.Size = new System.Drawing.Size(62, 23);
+            this.lblCameraId.TabIndex = 8;
+            this.lblCameraId.Text = "CameraId";
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.Location = new System.Drawing.Point(12, 197);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(75, 20);
+            this.lblFrom.TabIndex = 15;
+            this.lblFrom.Text = "From";
+            // 
+            // lblTo
+            // 
+            this.lblTo.Location = new System.Drawing.Point(12, 250);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(100, 17);
+            this.lblTo.TabIndex = 17;
+            this.lblTo.Text = "To";
+            // 
+            // grpPTZ
+            // 
+            this.grpPTZ.Controls.Add(this.btnPTZUp);
+            this.grpPTZ.Controls.Add(this.btnPTZDown);
+            this.grpPTZ.Controls.Add(this.btnPTZLeft);
+            this.grpPTZ.Controls.Add(this.btnPTZRight);
+            this.grpPTZ.Controls.Add(this.btnZoomIn);
+            this.grpPTZ.Controls.Add(this.btnZoomOut);
+            this.grpPTZ.Location = new System.Drawing.Point(20, 310);
+            this.grpPTZ.Name = "grpPTZ";
+            this.grpPTZ.Size = new System.Drawing.Size(150, 170);
+            this.grpPTZ.TabIndex = 20;
+            this.grpPTZ.TabStop = false;
+            this.grpPTZ.Text = "PTZ Control";
+            // 
+            // btnPTZUp
+            // 
+            this.btnPTZUp.Location = new System.Drawing.Point(55, 25);
+            this.btnPTZUp.Name = "btnPTZUp";
+            this.btnPTZUp.Size = new System.Drawing.Size(40, 30);
+            this.btnPTZUp.TabIndex = 0;
+            this.btnPTZUp.Text = "▲";
+            // 
+            // btnPTZDown
+            // 
+            this.btnPTZDown.Location = new System.Drawing.Point(55, 95);
+            this.btnPTZDown.Name = "btnPTZDown";
+            this.btnPTZDown.Size = new System.Drawing.Size(40, 30);
+            this.btnPTZDown.TabIndex = 1;
+            this.btnPTZDown.Text = "▼";
+            // 
+            // btnPTZLeft
+            // 
+            this.btnPTZLeft.Location = new System.Drawing.Point(15, 60);
+            this.btnPTZLeft.Name = "btnPTZLeft";
+            this.btnPTZLeft.Size = new System.Drawing.Size(40, 30);
+            this.btnPTZLeft.TabIndex = 2;
+            this.btnPTZLeft.Text = "◄";
+            // 
+            // btnPTZRight
+            // 
+            this.btnPTZRight.Location = new System.Drawing.Point(95, 60);
+            this.btnPTZRight.Name = "btnPTZRight";
+            this.btnPTZRight.Size = new System.Drawing.Size(40, 30);
+            this.btnPTZRight.TabIndex = 3;
+            this.btnPTZRight.Text = "►";
+            // 
+            // btnZoomIn
+            // 
+            this.btnZoomIn.Location = new System.Drawing.Point(20, 130);
+            this.btnZoomIn.Name = "btnZoomIn";
+            this.btnZoomIn.Size = new System.Drawing.Size(45, 25);
+            this.btnZoomIn.TabIndex = 4;
+            this.btnZoomIn.Text = "+";
+            // 
+            // btnZoomOut
+            // 
+            this.btnZoomOut.Location = new System.Drawing.Point(80, 130);
+            this.btnZoomOut.Name = "btnZoomOut";
+            this.btnZoomOut.Size = new System.Drawing.Size(45, 25);
+            this.btnZoomOut.TabIndex = 5;
+            this.btnZoomOut.Text = "-";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(850, 492);
+            this.Controls.Add(this.lblBaseUrl);
+            this.Controls.Add(this.txtBaseUrl);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.lblCameraId);
+            this.Controls.Add(this.cmbCameraId);
+            this.Controls.Add(this.btnPlayback);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnPlayLive);
+            this.Controls.Add(this.btnStartListener);
+            this.Controls.Add(this.btnStopListener);
+            this.Controls.Add(this.lblFrom);
+            this.Controls.Add(this.dtFrom);
+            this.Controls.Add(this.lblTo);
+            this.Controls.Add(this.dtTo);
+            this.Controls.Add(this.videoPanel);
+            this.Controls.Add(this.grpPTZ);
+            this.Name = "Form1";
+            this.Text = "NVR Client";
+            this.grpPTZ.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TextBox txtBaseUrl;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.ComboBox cmbCameraId;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnPlayback;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnPlayLive;
+        private System.Windows.Forms.Button btnStartListener;
+        private System.Windows.Forms.Button btnStopListener;
+        private System.Windows.Forms.Panel videoPanel;
+        private System.Windows.Forms.DateTimePicker dtFrom;
+        private System.Windows.Forms.DateTimePicker dtTo;
+        private System.Windows.Forms.Label lblBaseUrl;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblCameraId;
+        private System.Windows.Forms.Label lblFrom;
+        private System.Windows.Forms.Label lblTo;
+        private System.Windows.Forms.GroupBox grpPTZ;
+        private System.Windows.Forms.Button btnPTZUp;
+        private System.Windows.Forms.Button btnPTZDown;
+        private System.Windows.Forms.Button btnPTZLeft;
+        private System.Windows.Forms.Button btnPTZRight;
+        private System.Windows.Forms.Button btnZoomIn;
+        private System.Windows.Forms.Button btnZoomOut;
+    }
+}
---
+++ NVRClient/Form1.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+using System.Xml.Linq;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+// VLC
+using LibVLCSharp.Shared;
+using LibVLCSharp.WinForms;
+
+namespace NVRClient
+{
+    public partial class Form1 : Form
+    {
+        private readonly string logFile =
+            Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "nvr_client.log");
+
+        // VLC
+        private LibVLC _libVLC;
+        private MediaPlayer _mediaPlayer;
+        private VideoView _videoView;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ApplyStyle();
+            InitVLC();
+
+            // Force attach buttons (designer safe)
+            btnPlayLive.Click += btnPlayLive_Click;
+            btnPlayback.Click += btnPlayback_Click;
+
+            Log("Application started");
+        }
+
+        // ===============================
+        // UI STYLE
+        // ===============================
+        private void ApplyStyle()
+        {
+            this.BackColor = Color.FromArgb(245, 247, 250);
+            this.Font = new Font("Segoe UI", 9F);
+
+            foreach (Control c in this.Controls)
+            {
+                if (c is Button btn)
+                {
+                    btn.FlatStyle = FlatStyle.Flat;
+                    btn.FlatAppearance.BorderSize = 0;
+                    btn.Height = 28;
+                    btn.ForeColor = Color.White;
+                    btn.BackColor = Color.FromArgb(52, 152, 219);
+                }
+            }
+
+            btnDisconnect.BackColor = Color.FromArgb(231, 76, 60);
+            btnExport.BackColor = Color.FromArgb(46, 204, 113);
+            btnStartListener.BackColor = Color.FromArgb(155, 89, 182);
+            btnStopListener.BackColor = Color.FromArgb(127, 140, 141);
+
+            if (grpPTZ != null)
+            {
+                grpPTZ.ForeColor = Color.FromArgb(60, 60, 60);
+                foreach (Control c in grpPTZ.Controls)
+                {
+                    if (c is Button btn)
+                    {
+                        btn.BackColor = Color.FromArgb(52, 73, 94);
+                        btn.ForeColor = Color.White;
+                        btn.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+                    }
+                }
+            }
+
+            videoPanel.BackColor = Color.Black;
+        }
+
+        // ===============================
+        // VLC INIT (32-bit)
+        // ===============================
+        private void InitVLC()
+        {
+            string vlcPath = @"C:\Program Files (x86)\VideoLAN\VLC";
+
+            if (!Directory.Exists(vlcPath))
+                throw new Exception("32-bit VLC not found at: " + vlcPath);
+
+            Core.Initialize(vlcPath);
+
+            _libVLC = new LibVLC();
+            _mediaPlayer = new MediaPlayer(_libVLC);
+
+            _videoView = new VideoView
+            {
+                MediaPlayer = _mediaPlayer,
+                Dock = DockStyle.Fill
+            };
+
+            videoPanel.Controls.Clear();
+            videoPanel.Controls.Add(_videoView);
+        }
+
+        // ===============================
+        // DESIGNER REQUIRED
+        // ===============================
+        private void lblBaseUrl_Click(object sender, EventArgs e) { }
+        private void btnConnect_Click(object sender, EventArgs e) { }
+
+        // ===============================
+        // CONNECT → FETCH CHANNELS
+        // ===============================
+        private void btnConnect_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                LoadNvrChannels();
+                MessageBox.Show("Connected & channels loaded");
+            }
+            catch (Exception ex)
+            {
+                Log(ex.ToString());
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // ===============================
+        // BASE URI
+        // ===============================
+        private Uri GetSafeBaseUri()
+        {
+            string baseUrl = txtBaseUrl.Text.Trim();
+
+            if (!baseUrl.StartsWith("http://") && !baseUrl.StartsWith("https://"))
+                baseUrl = "http://" + baseUrl;
+
+            if (!baseUrl.EndsWith("/"))
+                baseUrl += "/";
+
+            return new Uri(baseUrl);
+        }
+
+        // ===============================
+        // FETCH CHANNELS
+        // ===============================
+        private void LoadNvrChannels()
+        {
+            cmbCameraId.Items.Clear();
+
+            var handler = new HttpClientHandler
+            {
+                Credentials = new NetworkCredential(
+                    txtUsername.Text.Trim(),
+                    txtPassword.Text
+                ),
+                PreAuthenticate = true
+            };
+
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = GetSafeBaseUri();
+
+                var response = client
+                    .GetAsync("pramaAPI/ContentMgmt/InputProxy/channels/status")
+                    .Result;
+
+                response.EnsureSuccessStatusCode();
+
+                string xml = Encoding.UTF8.GetString(
+                    response.Content.ReadAsByteArrayAsync().Result
+                );
+
+                XDocument doc = XDocument.Parse(xml);
+
+                var channels = doc.Descendants()
+                    .Where(x => x.Name.LocalName == "InputProxyChannelStatus");
+
+                foreach (var ch in channels)
+                {
+                    string id = ch.Elements()
+                        .FirstOrDefault(e => e.Name.LocalName == "id")?.Value;
+
+                    string online = ch.Elements()
+                        .FirstOrDefault(e => e.Name.LocalName == "online")?.Value;
+
+                    if (!string.IsNullOrEmpty(id))
+                        cmbCameraId.Items.Add(id + " | Online: " + online);
+                }
+            }
+        }
+
+        // ===============================
+        // ▶ LIVE VIEW
+        // ===============================
+        private void btnPlayLive_Click(object sender, EventArgs e)
+        {
+            if (cmbCameraId.SelectedItem == null)
+            {
+                MessageBox.Show("Select camera first");
+                return;
+            }
+
+            int channel = int.Parse(
+                cmbCameraId.SelectedItem.ToString().Split('|')[0].Trim()
+            );
+
+            int rtspChannel = (channel * 100) + 1;
+
+            string ip = txtBaseUrl.Text
+                .Replace("http://", "")
+                .Replace("https://", "");
+
+            string rtsp =
+                $"rtsp://{txtUsername.Text}:{txtPassword.Text}@{ip}:554/Streaming/Channels/{rtspChannel}";
+
+            Log("LIVE RTSP: " + rtsp);
+
+            using (var media = new Media(_libVLC, rtsp, FromType.FromLocation))
+                _mediaPlayer.Play(media);
+        }
+
+        // ===============================
+        // ▶ PLAYBACK BUTTON
+        // ===============================
+        private void btnPlayback_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cmbCameraId.SelectedItem == null)
+                    throw new Exception("Select camera first");
+
+                int channel = int.Parse(
+                    cmbCameraId.SelectedItem.ToString().Split('|')[0].Trim()
+                );
+
+                var (start, end) = GetFromToTime();
+
+                if (end <= start)
+                    throw new Exception("End time must be after start time");
+
+                SearchPlayback(channel, start, end);
+            }
+            catch (Exception ex)
+            {
+                Log(ex.ToString());
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // ===============================
+        // GET FROM / TO DATETIME (NAME SAFE)
+        // ===============================
+        private (DateTime from, DateTime to) GetFromToTime()
+        {
+            var pickers = this.Controls
+                .OfType<DateTimePicker>()
+                .OrderBy(p => p.TabIndex)
+                .ToList();
+
+            if (pickers.Count < 2)
+                throw new Exception("From / To DateTimePickers not found");
+
+            return (pickers[0].Value, pickers[1].Value);
+        }
+
+        // ===============================
+        // PLAYBACK SEARCH API
+        // ===============================
+        private void SearchPlayback(int channel, DateTime start, DateTime end)
+        {
+            int trackId = (channel * 100) + 1;
+
+            string xml = $@"<?xml version=""1.0"" encoding=""utf-8""?>
+<CMSearchDescription>
+  <searchID>{Guid.NewGuid()}</searchID>
+  <trackList>
+    <trackID>{trackId}</trackID>
+  </trackList>
+  <timeSpanList>
+    <timeSpan>
+      <startTime>{start.ToUniversalTime():yyyy-MM-ddTHH:mm:ssZ}</startTime>
+      <endTime>{end.ToUniversalTime():yyyy-MM-ddTHH:mm:ssZ}</endTime>
+    </timeSpan>
+  </timeSpanList>
+  <maxResults>100</maxResults>
+  <searchResultPostion>0</searchResultPostion>
+</CMSearchDescription>";
+
+            Log("Playback Search XML:\n" + xml);
+
+            var handler = new HttpClientHandler
+            {
+                Credentials = new NetworkCredential(
+                    txtUsername.Text.Trim(),
+                    txtPassword.Text
+                ),
+                PreAuthenticate = true
+            };
+
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = GetSafeBaseUri();
+                var content = new StringContent(xml, Encoding.UTF8, "application/xml");
+                client.PostAsync("pramaAPI/ContentMgmt/search", content).Wait();
+            }
+
+            PlayPlayback(trackId, start, end);
+        }
+
+        // ===============================
+        // ▶ PLAYBACK RTSP
+        // ===============================
+        private void PlayPlayback(int trackId, DateTime start, DateTime end)
+        {
+            string ip = txtBaseUrl.Text
+                .Replace("http://", "")
+                .Replace("https://", "");
+
+            string s = start.ToUniversalTime().ToString("yyyyMMddTHHmmssZ");
+            string e = end.ToUniversalTime().ToString("yyyyMMddTHHmmssZ");
+
+            string rtsp =
+                $"rtsp://{txtUsername.Text}:{txtPassword.Text}@{ip}:554/Streaming/tracks/{trackId}" +
+                $"?starttime={s}&endtime={e}";
+
+            Log("PLAYBACK RTSP: " + rtsp);
+
+            using (var media = new Media(_libVLC, rtsp, FromType.FromLocation))
+                _mediaPlayer.Play(media);
+        }
+
+        // ===============================
+        // LOGGING
+        // ===============================
+        private void Log(string msg)
+        {
+            File.AppendAllText(
+                logFile,
+                $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {msg}\r\n"
+            );
+        }
+    }
+}
